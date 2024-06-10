2d962f98b2acaae73588b6bb0ca902878144f573
haruhi <shanghaoxi@live.com>
2024-06-10 22:10:48 +0800

    godot choose player and sahder

---
+++ Levels/ChosePlayer.gd
@@ -0,0 +1,14 @@
+extends Node
+
+
+func _input(event: InputEvent) -> void:
+	
+	pass
+
+func _on_h_split_container_focus_entered() -> void:
+	
+	pass # Replace with function body.
+
+
+func _on_h_split_container_focus_exited() -> void:
+	pass # Replace with function body.
--- Scripts/SignalBus.gd
+++ Scripts/SignalBus.gd
@@ -1,3 +1,6 @@
 extends Node
 
+# Global Signal Scripts
+# Signal Instance, Shared by all instance
+
 signal enemy_death(enemy :Enemy)
