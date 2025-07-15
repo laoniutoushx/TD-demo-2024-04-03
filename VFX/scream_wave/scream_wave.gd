aff6dacc4a249a1346927e7b3cc923cd248050d3
shanghx <shanghaoxi@seatone.com>
2025-07-15 09:40:25 +0800

    vfx

--- VFX/scream_wave/scream_wave.gd
+++ VFX/scream_wave/scream_wave.gd
@@ -1,9 +1,22 @@
 extends Node3D
 
 
-@onready var _pal: GPUParticles3D = $DoubleRingCore
+@onready var _ap: AnimationPlayer = $AnimationPlayer
+
+
 
 func _ready() -> void:
-    _pal.emitting = true
+    if _ap:
+        _ap.play("burning")
 
 
+
+
+func _on_animation_player_animation_finished(anim_name:StringName) -> void:
+    if anim_name == "burning":
+        CommonUtil.delay_execution(1.8, 
+        (func(_self) -> void: 
+            if is_instance_valid(_self):
+                _self.queue_free()
+        ).bind(self)
+        )
