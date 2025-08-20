ae7b793739bc2d4dda8784ca289fb3034fa60120
shanghx <shanghaoxi@seatone.com>
2025-08-20 17:29:29 +0800

    cooldown refresh talent

--- System/talent_system/resources/cooldown_talent.gd
+++ System/talent_system/resources/cooldown_talent.gd
@@ -1,1 +1,22 @@
-extends Node
+extends Node3D
+
+
+
+func action(talent_context: TalentContext) -> void:
+    # 播放施法动画 & 声音
+    var talent: Talent = talent_context.talent
+    var source_unit: BaseUnit = talent_context.source
+    var target_unit: BaseUnit = talent_context.target
+
+
+
+    # 获取节点树中所有 group 为 friend 的节点
+    var friends = get_tree().get_nodes_in_group("friend")
+
+    for friend in friends:
+        if friend is Turret:
+            print(friend.title)
+
+
+
+
