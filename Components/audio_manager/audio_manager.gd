eaa245a535b286bd0cfa17e24b602acd23cb5ae4
shanghx <shanghaoxi@seatone.com>
2025-09-03 17:31:06 +0800

    audio

---
+++ Components/audio_manager/audio_manager.gd
@@ -0,0 +1,4 @@
+extends Node3D
+
+
+@onready var bg_player: AudioStreamPlayer3D = %BGPlayer
--- Levels/setting/setting.gd
+++ Levels/setting/setting.gd
@@ -2,11 +2,21 @@ extends Control
 
 
 
+@onready var bg_volum: HSlider = %BgVolum
+@onready var sound_volum: HSlider = %SoundVolum
+@onready var lang_opt: OptionButton = %LangOpt
+@onready var gra_opt: OptionButton = %GraOpt
+@onready var apply: Button = %Apply
+@onready var rt: Button = %Return
+
 
 
 func _on_return_pressed() -> void:
 	pass # Replace with function body.
 
 
+
+
 func _on_apply_pressed() -> void:
 	pass # Replace with function body.
+
